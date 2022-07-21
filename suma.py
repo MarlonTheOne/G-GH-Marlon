924a9a5318b910aa54b38d1f1b72581c8c585cf2
NetAcad DEVASC <changeme@example.com>
2022-07-21 01:20:11 +0000

    Primer commit

---
+++ suma.py
@@ -0,0 +1,8 @@
+print("Ingresa los valores para realizar la suma...")
+
+value1 = int(input("Ingresa el primer valor: "))
+value2 = int(input("Ingresa el segundo valor: "))
+
+result = value1 + value2
+
+print(f"Tu resultado es {result}")
