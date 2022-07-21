ff4951bca2a43ca4fd621b8826fa3ec708a1f216
NetAcad DEVASC <changeme@example.com>
2022-07-21 01:41:54 +0000

    Tercer commit (rama programador2)

--- suma.py
+++ suma.py
@@ -4,6 +4,6 @@ value1 = int(input("Ingresa el primer valor: "))
 value2 = int(input("Ingresa el segundo valor: "))
 value3 = int(input("Ingresa el tercer valor: "))
 
-result = value1 + value2 + value3
+result = value1 - value2 - value3
 
 print(f"Tu resultado es {result}")
